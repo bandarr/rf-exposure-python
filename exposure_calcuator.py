4131b68a9da729c74776317a0671c91a55f6cef0
Dan Barr <dan.barr@outlook.com>
2022-01-30 20:05:31 -0500

    refactoring multiple methods

--- exposure_calcuator.py
+++ exposure_calcuator.py
@@ -4,52 +4,64 @@ import math
 
 
 class FrequencyValues:
-    def __init__(self, freq, swr, gaindbi):
+    def __init__( self, freq: float, swr: float, gaindbi: float ):
         self.freq = freq
         self.swr = swr
         self.gaindbi = gaindbi
 
+class CableValues:
+    def __init__( self, k1:int, k2:int ):
+        self.k1 = k1
+        self.k2 = k2
+
 class ExposureCalculator:
     
-    def __calculate_reflection_coefficient(  self, freq_values ):
-        return abs( (freq_values.swr - 1)/(freq_values.swr + 1) )
+    def __calculate_reflection_coefficient( self, freq_values: FrequencyValues ):
+        return abs( ( freq_values.swr - 1 )/( freq_values.swr + 1 ) )
 
-    def __get_matched_load_fraction(self, feedline_length, feedline_loss_100):
-        feedline_loss_for_matched_load = (feedline_length/100) * feedline_loss_100
-        return 10**((-feedline_loss_for_matched_load )/10)
+    def __calculate_feedline_loss_for_matched_load_at_frequency( self, feedline_length: int, feedline_loss_per_100ft_at_frequency: float ) -> float:
+        return ( feedline_length/100 ) * feedline_loss_per_100ft_at_frequency
+
+    def __calculate_feedline_loss_for_matched_load_at_frequency_percentage(self, feedline_loss_for_matched_load: float ) -> float:
+        return 10**( -feedline_loss_for_matched_load/10 ) 
+
+    def __calculate_feedline_loss_per_100ft_at_frequency( self, freq_values: FrequencyValues, cable_values: CableValues ) -> float:
+        return cable_values.k1 * math.sqrt( freq_values.freq + cable_values.k2 * freq_values.freq ) 
+
+    def __calculate_feedline_loss_for_swr (self, feedline_loss_for_matched_load_percentage: float, gamma_squared: float ) -> float:
+        return -10 * math.log10( feedline_loss_for_matched_load_percentage * ( ( 1 - gamma_squared)/( 1 - feedline_loss_for_matched_load_percentage**2 * gamma_squared ) ) )
+
+    def __calculate_feedline_loss_for_swr_percentage( self, feedline_loss_for_swr: float ) -> float:
+        return ( 100 - 100/( 10**( feedline_loss_for_swr/10 ) ) )/100
 
 
-
-    def calculate_uncontrolled_safe_distance( self, freq_values, xmtr_power, feedline_loss_100, feedline_length, duty_cycle, uncontrolled_percentage_30):
+    def calculate_uncontrolled_safe_distance( self, freq_values: FrequencyValues, cable_values: CableValues, transmitter_power: int, feedline_length: int, duty_cycle: float, uncontrolled_percentage_30_minutes: float ) -> float:
         gamma = self.__calculate_reflection_coefficient( freq_values )
-        
-        matched_load_fraction = self.__get_matched_load_fraction( feedline_length, feedline_loss_100 )
 
-        gamma_squared = abs(gamma)**2
+        feedline_loss_per_100ft_at_frequency = self.__calculate_feedline_loss_per_100ft_at_frequency( freq_values, cable_values )
 
-        one_minus_gamma_squared = 1 - gamma_squared
+        feedline_loss_for_matched_load_at_frequency = self.__calculate_feedline_loss_for_matched_load_at_frequency( feedline_length, feedline_loss_per_100ft_at_frequency )
 
-        denominator = 1 - (matched_load_fraction**2) * gamma_squared
+        feedline_loss_for_matched_load_at_frequency_percentage = self.__calculate_feedline_loss_for_matched_load_at_frequency_percentage( feedline_loss_for_matched_load_at_frequency)
 
-        feed_line_loss = -10 * math.log10( matched_load_fraction * (one_minus_gamma_squared/denominator) )
+        gamma_squared = abs( gamma )**2
 
-        loss_percentage = (100 - 100/(10**(feed_line_loss/10)))/100
+        feedline_loss_for_swr = self.__calculate_feedline_loss_for_swr( feedline_loss_for_matched_load_at_frequency_percentage, gamma_squared )
 
-        power_loss = loss_percentage * xmtr_power
+        feedline_loss_for_swr_percentage = self.__calculate_feedline_loss_for_swr_percentage( feedline_loss_for_swr )
 
-        pep_at_antenna = xmtr_power - power_loss
+        power_loss_at_swr = feedline_loss_for_swr_percentage * transmitter_power
 
-        uncontrolled_average_pep = pep_at_antenna * duty_cycle * uncontrolled_percentage_30
+        peak_envelope_power_at_antenna = transmitter_power - power_loss_at_swr
 
-        mpe_s = 180/(freq_values.freq**2)
+        uncontrolled_average_pep = peak_envelope_power_at_antenna * duty_cycle * uncontrolled_percentage_30_minutes
 
-        gain_decimal = 10**(freq_values.gaindbi/10)
+        mpe_s = 180/( freq_values.freq**2 )
 
-        uncontrolled_safe_distance = math.sqrt((0.219*uncontrolled_average_pep * gain_decimal)/mpe_s)
+        gain_decimal = 10**( freq_values.gaindbi/10 )
 
-        return uncontrolled_safe_distance
+        #TODO:  Include effects of ground reflections
 
-
-
-    
+        return math.sqrt( ( 0.219 * uncontrolled_average_pep * gain_decimal )/mpe_s )
+ 
         
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from exposure_calcuator import *
 
 def main():
-    f1 = FrequencyValues(7.3, 2.25, 1.5)
+    c1 = CableValues(0.122290, 0.000260)  #for LMR-400
     xmtr_power = 1000
     feed_line_length = 113
-    loss_per_100 = 0.5
     duty_cycle = .5
     per_30 = .5
 
     ec1 = ExposureCalculator()
 
-    yarg = ec1.calculate_uncontrolled_safe_distance( f1, xmtr_power, loss_per_100, feed_line_length, duty_cycle, per_30 )
+    freq_values = [ FrequencyValues(7.3, 2.25, 1.5), FrequencyValues(14.35, 1.35, 1.5), FrequencyValues(18.1, 3.7, 1.5), FrequencyValues(21.45, 4.45, 1.5), FrequencyValues(24.99, 4.1, 1.5), FrequencyValues(29.7, 2.18, 4.5)]
     
-    print(yarg)
+    for f in freq_values:
+        yarg = ec1.calculate_uncontrolled_safe_distance( f, c1, xmtr_power, feed_line_length, duty_cycle, per_30 )
+        print(yarg)
 
 if __name__ == "__main__":
     main()
