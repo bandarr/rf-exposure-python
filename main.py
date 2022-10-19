775023b14df11fa51671bb2d96bcaaf76d68694b
Dan Barr <dan.barr@outlook.com>
2022-10-18 20:07:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def main():
 
     ec1 = ExposureCalculator()
 
-    freq_values = [ FrequencyValues(7.3, 2.25, 1.5), FrequencyValues(14.35, 1.35, 1.5), FrequencyValues(18.1, 3.7, 1.5), FrequencyValues(21.45, 4.45, 1.5), FrequencyValues(24.99, 4.1, 1.5), FrequencyValues(29.7, 2.18, 4.5)]
+    all_freq_values = [ FrequencyValues(7.3, 2.25, 1.5), FrequencyValues(14.35, 1.35, 1.5), FrequencyValues(18.1, 3.7, 1.5), FrequencyValues(21.45, 4.45, 1.5), FrequencyValues(24.99, 4.1, 1.5), FrequencyValues(29.7, 2.18, 4.5)]
     
-    for f in freq_values:
+    for f in all_freq_values:
         yarg = ec1.calculate_uncontrolled_safe_distance( f, c1, xmtr_power, feed_line_length, duty_cycle, per_30 )
         print(yarg)
 
